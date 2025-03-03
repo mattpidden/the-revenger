b78e4533e003d0060db7b29065069b475dc21597
Matthew Pidden <bb22475@bristol.ac.uk>
2025-03-03 23:54:42 +0000

    full working Thistlethwaite 4 phase method for 3x3 reductiongit add .!

--- code/solver/main.cpp
+++ code/solver/main.cpp
@@ -132,8 +132,7 @@ int main() {
     Phase phase6("Phase 6", {R, L, F2, B2, U, D}, {0,1,2,3,4,7,8,11,12,13,14,15,36,39,40,43,68,71,72,75,80,81,82,83,84,87,88,91,92,93,94,95}, {{-3, {0,1,2,3,4,7,8,11,12,13,14,15,80,81,82,83,84,87,88,91,92,93,94,95}}, {-4, {36,39,40,43,68,71,72,75}}}, "phase6table.bin", 13);
     Phase phase7("Phase 7", {R2, L2, F2, B2, U, D}, {0,3,12,15,16,17,18,19,20,23,24,27,28,29,30,31,32,33,34,35,36,39,40,43,44,45,46,47,48,49,50,51,52,55,56,59,60,61,62,63,64,65,66,67,68,71,72,75,76,77,78,79,80,83,92,95}, {{-3, {17,18,20,23,24,27,29,30,49,50,52,55,56,59,61,62}}, {-4, {33,34,36,39,40,43,45,46,65,66,68,71,72,75,77,78}}}, "phase7table.bin", 14);
     Phase phase8("Phase 8", {R2, L2, F2, B2, U2, D2}, {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95}, {}, "phase8table.bin", 15);
-
-    std::array<Phase, 3> phases = {phase5, phase6, phase7};
+    std::array<Phase, 4> phases = {phase5, phase6, phase7, phase8};
  
     //cube.import_state("WYYWYWWWYWWWWYYWOOOOOOOROOORRRRRGGGGGGGBGGGBBBBBROORRRRORRROORROBBBBBBBGBBBGGGGGYWWYWYYYWYYYYWWY");
     std::vector<Move> scramble = cube.apply_random_moves(35, {R, L, U, D, F, B});
@@ -142,9 +141,8 @@ int main() {
         std::cout << move_to_string(move) << " ";
         cube.move(move);
     }
+    Cube4x4 scrambled_cube = cube;
     cube.print();
-
-    
 
     std::vector<Move> solution = {};
     for (const Phase& phase : phases) {
@@ -159,7 +157,8 @@ int main() {
         }
         print_apply_solution(cube, phase_solution, phase.name);
     }
-    
-    cube.print();
+
+    print_apply_solution(scrambled_cube, solution, "Full");
+    scrambled_cube.print();
     return 0;
 }
